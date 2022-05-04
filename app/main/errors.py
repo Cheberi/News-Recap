77cb64a7fc5b35694f13c203bc2e011d2db31f47
Cheberi <meshackkemboi011@gmail.com>
2022-05-04 12:23:40 +0300

    styled the homepage

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,5 +2,5 @@ from flask import render_template
 from . import main
 
 @main.app_errorhandler(404)
-def four_Ow_four(error):
-    return render_template('four_Ow_four.html'),404
+def news(error):
+    return render_template('news.html'),404
