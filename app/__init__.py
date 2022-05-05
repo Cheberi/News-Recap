9b1a3be3d3c8acc2330efc262281afd70bdeb466
Cheberi <meshackkemboi011@gmail.com>
2022-05-05 09:02:29 +0300

    deploying to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app(config_name):
     app = Flask(__name__)
     
     app.config.from_object(config_options[config_name])
-    from .main import main as main_blueprint
+    from .main import main_blueprint
     app.register_blueprint(main_blueprint)
     
     
